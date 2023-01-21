f6feff64a7cf1c6311a887df742144763a65f725
RamiroBertran <ramirobertra@gmail.com>
2023-01-21 18:00:05 -0300

    index modified

--- script/main.js
+++ script/main.js
@@ -6,3 +6,14 @@ const myName = prompt("Select a new name");
 localStorage.setItem("name", myName); 
 myHeading.textContent = `Welcome ${myName}`; 
 }
+
+if(!localStorage.getItem){
+    setName();
+} else {
+    const name = localStorage.setItem("name"); 
+    myHeading.textContent = name;
+}
+
+myButton.onclick(() => {
+    setName();
+})
